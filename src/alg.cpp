1d790e466adbf482c6289ee544f0e845444fd912
Сигов Дмитрий <99177871+SigovDmitriy@users.noreply.github.com>
2022-04-02 18:12:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -76,8 +76,6 @@ int eval(std::string pref) {
             }
             if (tNum) {
                 stck1.push(std::stoi(temp));
-            }
-            else {
             } else {
                 mun = stck1.get();
                 stck1.pop();
