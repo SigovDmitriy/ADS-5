4da693d83293cc190fbe895385cf2136fb8d3121
Сигов Дмитрий <99177871+SigovDmitriy@users.noreply.github.com>
2022-04-02 19:47:16 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,8 +2,29 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename Type, int size>
 class TStack {
-
+private:
+Type arr[100];
+int top;
+public:
+Tstack() : top(-1) {}
+bool isEmpty() const {
+  return top == -1;
+}
+bool isFull() const {
+  return size == size - 1;
+}
+Type get() {
+  return arr[top];
+}
+void pop() {
+  if (top >= 0)
+    top--;
+  }
+  void push(Type value) {
+    if (top < size - 1)
+      arr[++top] = value;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
